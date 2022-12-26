cb0a01c19d4954c4ab03a44e2a68370b2a19b846
yifang1111 <yvonnewang@livemail.tw>
2022-12-26 16:39:25 +0800

    sa

---
+++ SA_optimization/scripts/download_replica.sh
@@ -0,0 +1,8 @@
+mkdir -p Datasets
+cd Datasets
+# you can also download the Replica.zip manually through
+# link: https://caiyun.139.com/m/i?1A5Ch5C3abNiL password: v3fY (the zip is split into smaller zips because of the size limitation of caiyun)
+# wget https://cvg-data.inf.ethz.ch/nice-slam/data/Replica.zip
+wget 'https://www.dropbox.com/s/z7dde30f6nhge00/room0.zip'
+
+unzip room0.zip
